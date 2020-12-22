b15064ec5516eb0341228a8856728ee88fde9f35
rajeshmr82 <rajesh.mr82@gmail.com>
2020-12-22 13:59:18 +0530

    Update Solution.java

--- Day1/Solution.java
+++ Day1/Solution.java
@@ -83,34 +83,6 @@ class AOC {
 
         return false;
     }
-
-    //Day 1 : Part 1
-    int sumOf2020(int[] input){
-        HashSet<Integer> set = new HashSet<>();
-        for (int i = 0; i <input.length; i++) {
-            if(set.contains(2020-input[i])){
-                return input[i]*(2020-input[i]);
-            }
-        }
-
-        return -1;
-    }
-
-    //Day 1: Part 2
-    int tripletSumOf2020(int[] input){
-        for (int i = 0; i <input.length-2; i++) {
-            HashSet<Integer> set = new HashSet<>();
-            int currSum = 2020 - input[i];
-            for (int j = i+1; j < input.length; j++) {
-                if(set.contains(currSum-input[j])){
-                    return input[i]*input[j]*(currSum-input[j]);
-                }
-                set.add(input[j]);
-            }
-        }
-
-        return -1;
-    }
 }
 
 
